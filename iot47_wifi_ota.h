759fb7bc168c6eeb150dff4d833b040264ce3574
DAO NGUYEN <54179901+daonguyen207@users.noreply.github.com>
2023-11-02 15:37:15 +0700

    Update iot47_wifi_ota.h

--- iot47_wifi_ota.h
+++ iot47_wifi_ota.h
@@ -3,68 +3,85 @@
 
 #include "ota_index_page.h"
 #include <ESPAsyncWebServer.h>
+#if defined(ESP8266)
+#include <Updater.h>
+#else
 #include <Update.h>
+#endif
 
 static char flag = 0;
+static uint32_t totol_size;
+static char show_debug=true;
+
+void iot47_ota_set_debug(bool mode)
+{
+   show_debug = mode;
+}
+
 int UpdateRun()
 {
-    Serial.println("OTA finished!");
-    Serial.printf("Hash in chip: %s\n", Update.md5String().c_str());
+    if(show_debug)Serial.println("OTA finished!");
+    if(show_debug)Serial.printf("Hash in chip: %s\n", Update.md5String().c_str());
     if (Update.isFinished())
     {
-        Serial.println("Restart device!");
+        if(show_debug)Serial.println("Restart device!");
         delay(2000);
         ESP.restart();
     }
     else
     {
-        Serial.println("OTA not fiished");
+        if(show_debug)Serial.println("OTA not fiished");
     }
 }
 void handleUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final)
 {
   if(!index)
   {
-    Serial.println("UploadStart");
-    Update.begin(UPDATE_SIZE_UNKNOWN);
+    if(request->hasHeader("Size"))
+    {
+      AsyncWebHeader* h = request->getHeader("Size");
+      totol_size = h->value().toInt();
+      Update.begin(totol_size);
+      if(show_debug)Serial.println("UploadStart: " + String(totol_size));
+    }
+    else
+    {
+      if(show_debug)Serial.println(F("No Size, update fail"));
+      request->send(400);
+    }
   }
   Update.write(data,len);
   if(final)
   {
-    if(request->hasHeader("Size"))
+    if(totol_size == (index+len))
     {
-      AsyncWebHeader* h = request->getHeader("Size");
-      uint32_t totol_size = h->value().toInt();
-      if(totol_size == (index+len))
+      if(request->hasHeader("Hash"))
       {
-        if(request->hasHeader("Hash"))
+        AsyncWebHeader* h = request->getHeader("Hash");
+        Update.setMD5((const char *)h->value().c_str());
+        if(show_debug)Serial.printf("Hash: %s\n", h->value().c_str());
+        if (Update.end(true))
         {
-          AsyncWebHeader* h = request->getHeader("Hash");
-          Update.setMD5((const char *)h->value().c_str());
-          Serial.printf("Hash: %s\n", h->value().c_str());
-          if (Update.end(true))
-          {
-            Serial.printf("\r\nUploadEnd: %s, %u B\n", filename.c_str(), totol_size);
-            flag = 1;
-          }
-          else
-          {
-            Serial.println(F("MD5 hash fail !"));
-            Serial.println("Error occured #: " + String(Update.getError()));
-            request->send(400,"MD5 error");
-          }       
+          if(show_debug)Serial.printf("\r\nUploadEnd: %s, %u B\n", filename.c_str(), totol_size);
+          flag = 1;
         }
         else
         {
-          Serial.println(F("No Hash, update fail"));
-          request->send(400);
-        }
+          if(show_debug)Serial.println(F("MD5 hash fail !"));
+          if(show_debug)Serial.println("Error occured #: " + String(Update.getError()));
+          request->send(400,"MD5 error");
+        }       
       }
       else
       {
-        Serial.println(F("Size firmware error"));
+        if(show_debug)Serial.println(F("No Hash, update fail"));
         request->send(400);
       }
+    }
+    else
+    {
+      if(show_debug)Serial.println(F("Size firmware error"));
+      request->send(400);
     }
   }
 }
